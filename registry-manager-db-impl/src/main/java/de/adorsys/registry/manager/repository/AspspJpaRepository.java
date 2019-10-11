5826bf6e8b7358aa074c362a1b35deb9f73c9a34
Oleksandr Berezkin <oleksandrberezkin@Oleksandrs-MacBook-Pro.local>
2019-10-11 13:37:43 +0300

    move Aspsp manager, set Aspsp id as UUID field, add checking if Aspsp id is null and update it

--- registry-manager-db-impl/src/main/java/de/adorsys/registry/manager/repository/AspspJpaRepository.java
+++ registry-manager-db-impl/src/main/java/de/adorsys/registry/manager/repository/AspspJpaRepository.java
@@ -1,9 +1,7 @@
 package de.adorsys.registry.manager.repository;
 
 import de.adorsys.registry.manager.repository.model.AspspEntity;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
