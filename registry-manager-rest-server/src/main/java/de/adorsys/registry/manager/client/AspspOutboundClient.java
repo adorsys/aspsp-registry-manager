9cc2aa6c1b053c00c591d62d4330ecc8e429138a
berezkin88 <berezkin05@gmail.com>
2019-10-31 16:21:09 +0200

    add Javadocs

--- registry-manager-rest-server/src/main/java/de/adorsys/registry/manager/client/AspspOutboundClient.java
+++ registry-manager-rest-server/src/main/java/de/adorsys/registry/manager/client/AspspOutboundClient.java
@@ -12,9 +12,19 @@ import org.springframework.web.multipart.MultipartFile;
     configuration = ClientConfiguration.class)
 public interface AspspOutboundClient {
 
+    /**
+     * Requests for data from Adapter for putting it into the existing database
+     *
+     * @return records as an array of bytes
+     */
     @RequestMapping(method = RequestMethod.GET, value = "/export")
     byte[] importFile();
 
+    /**
+     * Sends records from the current repository into Adapter
+     *
+     * @param file as a {@link MultipartFile}
+     */
     @RequestMapping(method = RequestMethod.POST, value = "/import", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     void exportFile(@Param("file") MultipartFile file);
 
