6f90741e4598631bc0b46d5bc5ce1632e5adaf61
berezkin88 <berezkin05@gmail.com>
2019-11-01 12:37:52 +0200

    add security to export/import methods, add tests for security measures

--- registry-manager-rest-server/src/main/java/de/adorsys/registry/manager/resource/AspspOutboundResource.java
+++ registry-manager-rest-server/src/main/java/de/adorsys/registry/manager/resource/AspspOutboundResource.java
@@ -2,7 +2,11 @@ package de.adorsys.registry.manager.resource;
 
 import de.adorsys.registry.manager.client.AspspOutboundClient;
 import de.adorsys.registry.manager.service.AspspOutboundService;
+import io.swagger.annotations.ApiOperation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
@@ -11,6 +15,7 @@ import java.io.IOException;
 @RequestMapping("/v1/aspsps/outbound")
 public class AspspOutboundResource {
 
+    private static final Logger logger = LoggerFactory.getLogger(AspspResource.class);
     private AspspOutboundService aspspOutboundService;
     private AspspOutboundClient aspspOutboundClient;
 
@@ -19,15 +24,23 @@ public class AspspOutboundResource {
         this.aspspOutboundClient = aspspOutboundClient;
     }
 
+    @PreAuthorize("hasRole('DEPLOYER')")
+    @ApiOperation("Export ASPSPs into Adapter")
     @PostMapping("/export")
     public ResponseEntity<Void> exportData() throws IOException {
+        logger.info("Export all ASPSPs into Adapter");
+
         aspspOutboundClient.exportFile(aspspOutboundService.exportData());
 
         return ResponseEntity.noContent().build();
     }
 
+    @PreAuthorize("hasRole('DEPLOYER')")
+    @ApiOperation("Import ASPSPs from Adapter")
     @GetMapping("/import")
     public ResponseEntity<Void> importData() {
+        logger.info("Import all ASPSPs from Adapter");
+
         aspspOutboundService.importData(aspspOutboundClient.importFile());
 
         return ResponseEntity.noContent().build();
--- registry-manager-rest-server/src/test/java/de/adorsys/registry/manager/resource/AspspOutboundResourceTest.java
+++ registry-manager-rest-server/src/test/java/de/adorsys/registry/manager/resource/AspspOutboundResourceTest.java
@@ -1,16 +1,24 @@
 package de.adorsys.registry.manager.resource;
 
 import de.adorsys.registry.manager.client.AspspOutboundClient;
+import de.adorsys.registry.manager.config.SecurityConfig;
 import de.adorsys.registry.manager.exception.ExceptionHandlingAdvisor;
 import de.adorsys.registry.manager.service.AspspOutboundService;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -20,32 +28,30 @@ import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
-@AutoConfigureMockMvc
+@RunWith(SpringRunner.class)
+@WebMvcTest(AspspOutboundResource.class)
+@Import(SecurityConfig.class)
 public class AspspOutboundResourceTest {
     private static final String BASE_URI = "/v1/aspsps/outbound";
 
+    @Autowired
     private MockMvc mockMvc;
 
-    @InjectMocks
-    private AspspOutboundResource resource;
-
-    @Mock
+    @MockBean
     private AspspOutboundService service;
 
-    @Mock
+    @MockBean
     private AspspOutboundClient client;
 
-    @Before
-    public void setUp() {
-        MockitoAnnotations.initMocks(this);
+    @Test
+    public void importDataRedirectToLoginPage() throws Exception {
 
-        mockMvc = MockMvcBuilders.standaloneSetup(resource)
-            .setMessageConverters()
-            .setControllerAdvice(new ExceptionHandlingAdvisor())
-            .build();
+        mockMvc.perform(MockMvcRequestBuilders.get(BASE_URI + "/import"))
+            .andExpect(status().is(HttpStatus.FOUND.value()))
+            .andReturn();
     }
 
+    @WithMockUser(roles = "DEPLOYER")
     @Test
     public void importData() throws Exception {
         doNothing().when(service).importData(any());
@@ -57,6 +63,24 @@ public class AspspOutboundResourceTest {
         verify(service, times(1)).importData(any());
     }
 
+    @WithMockUser(roles = {"READER", "MANAGER"})
+    @Test
+    public void importDataForbidden() throws Exception {
+
+        mockMvc.perform(MockMvcRequestBuilders.get(BASE_URI + "/import"))
+            .andExpect(status().is(HttpStatus.FORBIDDEN.value()))
+            .andReturn();
+    }
+
+    @Test
+    public void exportDataRedirectToLoginPage() throws Exception {
+
+        mockMvc.perform(MockMvcRequestBuilders.post(BASE_URI + "/export"))
+            .andExpect(status().is(HttpStatus.FOUND.value()))
+            .andReturn();
+    }
+
+    @WithMockUser(roles = "DEPLOYER")
     @Test
     public void exportData() throws Exception {
         doNothing().when(client).exportFile(any());
@@ -66,5 +90,14 @@ public class AspspOutboundResourceTest {
             .andReturn();
 
         verify(client, times(1)).exportFile(any());
+    }
+
+    @WithMockUser(roles = {"READER", "MANAGER"})
+    @Test
+    public void exportDataForbidden() throws Exception {
+
+        mockMvc.perform(MockMvcRequestBuilders.post(BASE_URI + "/export"))
+            .andExpect(status().is(HttpStatus.FORBIDDEN.value()))
+            .andReturn();
     }
 }
