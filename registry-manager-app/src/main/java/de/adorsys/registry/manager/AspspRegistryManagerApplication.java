9e7ff7493739904ccf6fb75c987715fd5d732e37
berezkin88 <berezkin05@gmail.com>
2019-11-11 12:59:48 +0200

    remove export and import buttons, remove feign from the back

--- registry-manager-app/src/main/java/de/adorsys/registry/manager/AspspRegistryManagerApplication.java
+++ registry-manager-app/src/main/java/de/adorsys/registry/manager/AspspRegistryManagerApplication.java
@@ -2,14 +2,12 @@ package de.adorsys.registry.manager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
 @EnableConfigurationProperties
 @EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
 @SpringBootApplication(scanBasePackages = "de.adorsys.registry.manager")
-@EnableFeignClients
 public class AspspRegistryManagerApplication {
     public static void main(String[] args) {
         SpringApplication.run(AspspRegistryManagerApplication.class, args); //NOSONAR
--- registry-manager-rest-server/src/main/resources/static/main.js
+++ registry-manager-rest-server/src/main/resources/static/main.js
@@ -180,33 +180,33 @@ function addTooltips(e) {
     let editId = "edit-";
     let updateId = "update-";
     let deleteId = "delete-";
-
+    
     if (e.className.indexOf("edit") > -1) {
         let helper = e.parentNode.childNodes[7];
-
+        
         e.addEventListener("click", () => { editButton(e) });
         e.setAttribute("id", editId + COUNTER);
-
+        
         helper.setAttribute("data-mdl-for", editId + COUNTER);
         helper.setAttribute("class", "mdl-tooltip mdl-tooltip--top");
     }
-
+    
     if (e.className.indexOf("update") > -1) {
         let helper = e.parentNode.childNodes[9];
-
+        
         e.addEventListener("click", () => { greenButton(e) });
         e.setAttribute("id", updateId + COUNTER);
-
+        
         helper.setAttribute("data-mdl-for", updateId + COUNTER);
         helper.setAttribute("class", "mdl-tooltip mdl-tooltip--top");
     }
-
+    
     if (e.className.indexOf("delete") > -1) {
         let helper = e.parentNode.childNodes[11];
-
+        
         e.addEventListener("click", () => { redButton(e) });
         e.setAttribute("id", deleteId + COUNTER);
-
+        
         helper.setAttribute("data-mdl-for", deleteId + COUNTER);
         helper.setAttribute("class", "mdl-tooltip mdl-tooltip--top");
     }
@@ -303,7 +303,7 @@ function buildRow(data) {
 function fail(message) {
     let messageBlock = FAILURE.querySelector(".message");
     messageBlock.textContent = message;
-
+    
     setTimeout(() => { FAILURE.style.opacity = 1 }, 500);
 
     setTimeout(() => { FAILURE.style.opacity = 0 }, 8000);
@@ -482,32 +482,6 @@ async function searchButton() {
     if (HIDDEN_ROW.parentElement.parentElement.parentElement.hidden) {
         showTable();
     }
-}
-
-function importButton() {
-    fetch("/v1/aspsps/adapter/import", {
-        method: 'POST'
-    }).then(response => {
-        if (!response.ok) {
-            throw Error(response.statusText);
-        }
-        success();
-    }).catch(() => {
-        fail("Failed to import data from Adapter");
-    })
-}
-
-function exportButton() {
-    fetch("/v1/aspsps/adapter/export", {
-        method: 'POST'
-    }).then(response => {
-        if (!response.ok) {
-            throw Error(response.statusText);
-        }
-        success();
-    }).catch(() => {
-        fail("Failed to export data into Adapter");
-    })
 }
 
 function mergeButton() {
