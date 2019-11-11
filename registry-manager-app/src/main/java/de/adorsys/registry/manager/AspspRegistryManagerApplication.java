d55cd320d1eff1d2a8a4d2b193bd5fca3fdb93d5
berezkin88 <berezkin05@gmail.com>
2019-11-11 19:59:32 +0200

    remove redundant annotation, improve writing into csv string function

--- registry-manager-app/src/main/java/de/adorsys/registry/manager/AspspRegistryManagerApplication.java
+++ registry-manager-app/src/main/java/de/adorsys/registry/manager/AspspRegistryManagerApplication.java
@@ -2,10 +2,8 @@ package de.adorsys.registry.manager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
-@EnableConfigurationProperties
 @EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
 @SpringBootApplication(scanBasePackages = "de.adorsys.registry.manager")
 public class AspspRegistryManagerApplication {
--- registry-manager-rest-server/src/main/resources/static/main.js
+++ registry-manager-rest-server/src/main/resources/static/main.js
@@ -180,33 +180,33 @@ function addTooltips(e) {
     let editId = "edit-";
     let updateId = "update-";
     let deleteId = "delete-";
-    
+
     if (e.className.indexOf("edit") > -1) {
         let helper = e.parentNode.childNodes[7];
-        
+
         e.addEventListener("click", () => { editButton(e) });
         e.setAttribute("id", editId + COUNTER);
-        
+
         helper.setAttribute("data-mdl-for", editId + COUNTER);
         helper.setAttribute("class", "mdl-tooltip mdl-tooltip--top");
     }
-    
+
     if (e.className.indexOf("update") > -1) {
         let helper = e.parentNode.childNodes[9];
-        
+
         e.addEventListener("click", () => { greenButton(e) });
         e.setAttribute("id", updateId + COUNTER);
-        
+
         helper.setAttribute("data-mdl-for", updateId + COUNTER);
         helper.setAttribute("class", "mdl-tooltip mdl-tooltip--top");
     }
-    
+
     if (e.className.indexOf("delete") > -1) {
         let helper = e.parentNode.childNodes[11];
-        
+
         e.addEventListener("click", () => { redButton(e) });
         e.setAttribute("id", deleteId + COUNTER);
-        
+
         helper.setAttribute("data-mdl-for", deleteId + COUNTER);
         helper.setAttribute("class", "mdl-tooltip mdl-tooltip--top");
     }
@@ -303,7 +303,7 @@ function buildRow(data) {
 function fail(message) {
     let messageBlock = FAILURE.querySelector(".message");
     messageBlock.textContent = message;
-    
+
     setTimeout(() => { FAILURE.style.opacity = 1 }, 500);
 
     setTimeout(() => { FAILURE.style.opacity = 0 }, 8000);
@@ -473,7 +473,7 @@ async function searchButton() {
         if (response.data.length === 0) {
             throw Error("no data");
         }
-        
+
         PAGINATOR.create(response.data, response.headers);
     } catch (error) {
         fail("Failed to find any records. Please double check the search conditions");
@@ -628,7 +628,7 @@ let PAGINATOR = {
     };
 
     PAGINATOR.addRow = (input) => {
-        for (let iterator = 0, limit = Math.min(PAGINATOR.size, PAGINATOR.left, input.length); iterator < limit; iterator++) {
+        for (let iterator = 0, limit = Math.min(PAGINATOR.size, input.length); iterator < limit; iterator++) {
             buildRow(input[iterator]);
             PAGINATOR.left--;
         }
