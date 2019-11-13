f62a4ba2b94bc035cca4b1a711b529c145c15e27
Alexander Berezkin <berezkin05@gmail.com>
2019-11-13 17:39:42 +0200

    Remove export-import features (#15)
    * move Aspsp manager, set Aspsp id as UUID field, add checking if Aspsp id is null and update it
    
    * typo fix
    
    * fix merge conflict
    
    * remove export and import buttons, remove feign from the back
    
    * remove redundant annotation, improve writing into csv string function
    
    * adjust peginator: remove redundant checking in the for loop, 'show more' button brings the right number of items to be displayed next
    
    * add missed thymeleaf schemas on the index

--- registry-manager-rest-server/src/main/resources/static/main.js
+++ registry-manager-rest-server/src/main/resources/static/main.js
@@ -128,7 +128,6 @@ function editableCells(e) {
 
     for (let i = 1, till = (rowCells.length - 2); i < till; i++) {
         rowCells[i].setAttribute("contenteditable", true);
-
     }
 
     approach.forEach(element => {
@@ -473,7 +472,7 @@ async function searchButton() {
         if (response.data.length === 0) {
             throw Error("no data");
         }
-        
+
         PAGINATOR.create(response.data, response.headers);
     } catch (error) {
         fail("Failed to find any records. Please double check the search conditions");
@@ -482,32 +481,6 @@ async function searchButton() {
     if (HIDDEN_ROW.parentElement.parentElement.parentElement.hidden) {
         showTable();
     }
-}
-
-function importButton() {
-    fetch("/v1/aspsps/adapter/import", {
-        method: 'POST'
-    }).then(response => {
-        if (!response.ok) {
-            throw Error(response.statusText);
-        }
-        success();
-    }).catch(() => {
-        fail("Failed to import data from Adapter");
-    })
-}
-
-function exportButton() {
-    fetch("/v1/aspsps/adapter/export", {
-        method: 'POST'
-    }).then(response => {
-        if (!response.ok) {
-            throw Error(response.statusText);
-        }
-        success();
-    }).catch(() => {
-        fail("Failed to export data into Adapter");
-    })
 }
 
 function mergeButton() {
@@ -648,17 +621,17 @@ let PAGINATOR = {
         PAGINATOR.total = document.querySelector(".total");
 
         PAGINATOR.total.innerHTML = dataLength;
-        PAGINATOR.button.innerHTML = "show next " + PAGINATOR.size;
 
         PAGINATOR.addRow(PAGINATOR.data);
     };
 
     PAGINATOR.addRow = (input) => {
-        for (let iterator = 0, limit = Math.min(PAGINATOR.size, PAGINATOR.left, input.length); iterator < limit; iterator++) {
+        for (let iterator = 0, limit = input.length; iterator < limit; iterator++) {
             buildRow(input[iterator]);
             PAGINATOR.left--;
         }
 
+        PAGINATOR.button.innerHTML = "show next " + Math.min(PAGINATOR.size, PAGINATOR.left);
         PAGINATOR.page++;
         PAGINATOR.showMore.hidden = PAGINATOR.left === 0;
     };
