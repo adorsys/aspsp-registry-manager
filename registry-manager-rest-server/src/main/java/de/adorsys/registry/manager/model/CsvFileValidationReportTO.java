88e4c1a0582cbefc122b9c6f5d3abe198aef0f4c
Mykola Shumyn <msh@adorsys.com.ua>
2019-11-14 12:39:59 +0200

    Added ObjectMapper configs

--- registry-manager-rest-server/src/main/java/de/adorsys/registry/manager/model/CsvFileValidationReportTO.java
+++ registry-manager-rest-server/src/main/java/de/adorsys/registry/manager/model/CsvFileValidationReportTO.java
@@ -1,10 +1,14 @@
 package de.adorsys.registry.manager.model;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 import java.util.List;
 
 public class CsvFileValidationReportTO {
     private ValidationResultTO validationResult;
     private Integer totalNotValidRecords;
+
+    @JsonInclude(JsonInclude.Include.NON_EMPTY)
     private List<AspspValidationReportTO> aspspValidationErrorReports;
 
     public ValidationResultTO getValidationResult() {
