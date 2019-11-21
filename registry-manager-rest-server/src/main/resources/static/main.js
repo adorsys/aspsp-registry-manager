98005928df83838a3988baa421081217ffd5ce38
Alexander Berezkin <berezkin05@gmail.com>
2019-11-21 17:03:28 +0200

    Front-end for import file validation (#22)
    * move Aspsp manager, set Aspsp id as UUID field, add checking if Aspsp id is null and update it
    
    * typo fix
    
    * fix merge conflict
    
    * add validation handler on the UI for resolving validation reports, add modal, spinner
    
    * add blank line for readability
    
    * adjust report() function

--- registry-manager-rest-server/src/main/resources/static/main.js
+++ registry-manager-rest-server/src/main/resources/static/main.js
@@ -1,9 +1,9 @@
 setTimeout(function () {
     initGlobals();
     document.querySelector("#upload>button").addEventListener("click", () => FILE_UPLOAD_FIELD.click());
-    FILE_UPLOAD_FIELD.addEventListener("change", upload);
+    FILE_UPLOAD_FIELD.addEventListener("change", validate);
     document.querySelector("#merge>button").addEventListener("click", () => FILE_MERGE_FIELD.click());
-    FILE_MERGE_FIELD.addEventListener("change", mergeButton);
+    FILE_MERGE_FIELD.addEventListener("change", merge);
     document.querySelectorAll(".mdl-textfield__input").forEach(field => field.addEventListener('keypress', event => onEnterPress(event)));
 }, 0);
 
@@ -17,11 +17,12 @@ function initGlobals() {
     window.COUNTER = 0;
     window.BASE = "/v1/aspsps";
     window.BASE_URL = "";
+    window.HIT_BUTTON = "NONE";
 }
 
 function validateBankName(element) {
     let target = element.textContent;
-    let regex = /^[\w\-\s\WäöüÄÖÜß]+$/;
+    let regex = /^[\w\s\WäöüÄÖÜß]+$/;
 
     if (!regex.test(target)) {
         element.classList.add("invalid");
@@ -303,6 +304,32 @@ function buildRow(data) {
 }
 // End of row part
 
+const toggleModal = () => {
+    const modal = document.querySelector(".validation-layout");
+    const spinner = document.querySelector(".spinner");
+    const verdict = document.querySelector(".verdict");
+    const amount = document.querySelector(".not-valid-amount");
+    const example = document.querySelector(".example");
+    const display = document.querySelector(".display");
+
+    verdict.classList.add("hidden");
+    amount.classList.add("hidden");
+    example.classList.add("hidden");
+    display.classList.add("hidden");
+
+    spinner.classList.remove("hidden");
+
+    modal.classList.toggle("hidden");
+}
+
+const createReport = (data) => {
+    const temp = new Blob([JSON.stringify(data)]);
+    const virtualLink = document.createElement("a");
+    const virtualUrl = URL.createObjectURL(temp);
+    virtualLink.href = virtualUrl;
+    virtualLink.download = "report.json";
+    virtualLink.click();
+}
 function fail(message) {
     let messageBlock = FAILURE.querySelector(".message");
     messageBlock.textContent = message;
@@ -381,7 +408,6 @@ function saveButton(e) {
         toggleButtons(e);
         return response.text();
     }).then(response => {
-        if (!response) { return; }
         let output = JSON.parse(response);
         row.cells[0].textContent = output.id;
         COUNTUP.update(COUNTUP.endVal + 1);
@@ -507,7 +533,7 @@ async function searchButton() {
     forceValidation();
 }
 
-function mergeButton() {
+function merge() {
     let file = FILE_MERGE_FIELD.files[0];
     let data = new FormData();
 
@@ -538,6 +564,33 @@ async function search(URI) {
     output.data = JSON.parse(await response.text());
 
     return output;
+}
+
+const validate = () => {
+    let file = FILE_UPLOAD_FIELD.files[0];
+    let data = new FormData();
+
+    data.append("file", file);
+
+    toggleModal();
+
+    fetch(BASE + "/csv/validate", {
+        method: 'POST',
+        body: data
+    }).then(response => {
+        if (response.status === 403) {
+            warning("It looks like you don't have enough permissions to perform this action");
+            return;
+        } else if (!response.ok && response.status !== 400) {
+            throw Error(response.statusText);
+        }
+        success();
+        return response.text()
+    }).then(response => {
+        validationResponseHandler(JSON.parse(response));
+    }).catch(() => {
+        fail("Validation process failed, please check if you provided an appropriately formatted CSV file");
+    })
 }
 // End of requests part
 
@@ -623,6 +676,23 @@ function showButton() {
     icon.classList.toggle("rotate");
 }
 
+const proceedButton = () => {
+    if (HIT_BUTTON === "UPLOAD") {
+        upload();
+    } else if (HIT_BUTTON === "MERGE") {
+        merge();
+    }
+
+    toggleModal();
+}
+
+const reportButton = () => {
+    createReport(VALIDATOR.data);
+} 
+
+const rejectButton = () => {
+    toggleModal();
+}
 let PAGINATOR = {
     data: null,
     page: 0,
@@ -681,3 +751,66 @@ let getTotal = async () => {
 // this counter is made by inorgaik https://inorganik.github.io/countUp.js/
 
 var __assign=this&&this.__assign||function(){return(__assign=Object.assign||function(t){for(var i,a=1,s=arguments.length;a<s;a++)for(var n in i=arguments[a])Object.prototype.hasOwnProperty.call(i,n)&&(t[n]=i[n]);return t}).apply(this,arguments)},CountUp=function(){function t(t,i,a){var s=this;this.target=t,this.endVal=i,this.options=a,this.version="2.0.4",this.defaults={startVal:0,decimalPlaces:0,duration:2,useEasing:!0,useGrouping:!0,smartEasingThreshold:999,smartEasingAmount:333,separator:",",decimal:".",prefix:"",suffix:""},this.finalEndVal=null,this.useEasing=!0,this.countDown=!1,this.error="",this.startVal=0,this.paused=!0,this.count=function(t){s.startTime||(s.startTime=t);var i=t-s.startTime;s.remaining=s.duration-i,s.useEasing?s.countDown?s.frameVal=s.startVal-s.easingFn(i,0,s.startVal-s.endVal,s.duration):s.frameVal=s.easingFn(i,s.startVal,s.endVal-s.startVal,s.duration):s.countDown?s.frameVal=s.startVal-(s.startVal-s.endVal)*(i/s.duration):s.frameVal=s.startVal+(s.endVal-s.startVal)*(i/s.duration),s.countDown?s.frameVal=s.frameVal<s.endVal?s.endVal:s.frameVal:s.frameVal=s.frameVal>s.endVal?s.endVal:s.frameVal,s.frameVal=Math.round(s.frameVal*s.decimalMult)/s.decimalMult,s.printValue(s.frameVal),i<s.duration?s.rAF=requestAnimationFrame(s.count):null!==s.finalEndVal?s.update(s.finalEndVal):s.callback&&s.callback()},this.formatNumber=function(t){var i,a,n,e,r,o=t<0?"-":"";if(i=Math.abs(t).toFixed(s.options.decimalPlaces),n=(a=(i+="").split("."))[0],e=a.length>1?s.options.decimal+a[1]:"",s.options.useGrouping){r="";for(var l=0,h=n.length;l<h;++l)0!==l&&l%3==0&&(r=s.options.separator+r),r=n[h-l-1]+r;n=r}return s.options.numerals&&s.options.numerals.length&&(n=n.replace(/[0-9]/g,function(t){return s.options.numerals[+t]}),e=e.replace(/[0-9]/g,function(t){return s.options.numerals[+t]})),o+s.options.prefix+n+e+s.options.suffix},this.easeOutExpo=function(t,i,a,s){return a*(1-Math.pow(2,-10*t/s))*1024/1023+i},this.options=__assign({},this.defaults,a),this.formattingFn=this.options.formattingFn?this.options.formattingFn:this.formatNumber,this.easingFn=this.options.easingFn?this.options.easingFn:this.easeOutExpo,this.startVal=this.validateValue(this.options.startVal),this.frameVal=this.startVal,this.endVal=this.validateValue(i),this.options.decimalPlaces=Math.max(this.options.decimalPlaces),this.decimalMult=Math.pow(10,this.options.decimalPlaces),this.resetDuration(),this.options.separator=String(this.options.separator),this.useEasing=this.options.useEasing,""===this.options.separator&&(this.options.useGrouping=!1),this.el="string"==typeof t?document.getElementById(t):t,this.el?this.printValue(this.startVal):this.error="[CountUp] target is null or undefined"}return t.prototype.determineDirectionAndSmartEasing=function(){var t=this.finalEndVal?this.finalEndVal:this.endVal;this.countDown=this.startVal>t;var i=t-this.startVal;if(Math.abs(i)>this.options.smartEasingThreshold){this.finalEndVal=t;var a=this.countDown?1:-1;this.endVal=t+a*this.options.smartEasingAmount,this.duration=this.duration/2}else this.endVal=t,this.finalEndVal=null;this.finalEndVal?this.useEasing=!1:this.useEasing=this.options.useEasing},t.prototype.start=function(t){this.error||(this.callback=t,this.duration>0?(this.determineDirectionAndSmartEasing(),this.paused=!1,this.rAF=requestAnimationFrame(this.count)):this.printValue(this.endVal))},t.prototype.pauseResume=function(){this.paused?(this.startTime=null,this.duration=this.remaining,this.startVal=this.frameVal,this.determineDirectionAndSmartEasing(),this.rAF=requestAnimationFrame(this.count)):cancelAnimationFrame(this.rAF),this.paused=!this.paused},t.prototype.reset=function(){cancelAnimationFrame(this.rAF),this.paused=!0,this.resetDuration(),this.startVal=this.validateValue(this.options.startVal),this.frameVal=this.startVal,this.printValue(this.startVal)},t.prototype.update=function(t){cancelAnimationFrame(this.rAF),this.startTime=null,this.endVal=this.validateValue(t),this.endVal!==this.frameVal&&(this.startVal=this.frameVal,this.finalEndVal||this.resetDuration(),this.determineDirectionAndSmartEasing(),this.rAF=requestAnimationFrame(this.count))},t.prototype.printValue=function(t){var i=this.formattingFn(t);"INPUT"===this.el.tagName?this.el.value=i:"text"===this.el.tagName||"tspan"===this.el.tagName?this.el.textContent=i:this.el.innerHTML=i},t.prototype.ensureNumber=function(t){return"number"==typeof t&&!isNaN(t)},t.prototype.validateValue=function(t){var i=Number(t);return this.ensureNumber(i)?i:(this.error="[CountUp] invalid start or end value: "+t,null)},t.prototype.resetDuration=function(){this.startTime=null,this.duration=1e3*Number(this.options.duration),this.remaining=this.duration},t}();
+
+const VALIDATOR = {
+    data: null
+}
+
+const validationResponseHandler = (data) => {
+    VALIDATOR.data = data;
+
+    let isValid = data.validationResult === "VALID";
+
+    const verdict = document.querySelector("#verdict");
+    const amount = document.querySelector("#records-amount");
+    const example = document.querySelector(".display");
+    const spinner = document.querySelector(".spinner");
+
+    if (!data) {
+        fail("Oops... something went wrong. Please try again to validate");
+        toggleModal();
+        return;
+    }
+    
+    verdict.textContent = data.validationResult;
+    spinner.classList.add("hidden");
+
+    if (!isValid) {
+        verdict.classList.add("not-valid");
+        verdict.parentElement.classList.remove("hidden");
+        amount.parentElement.classList.remove("hidden");
+        example.classList.remove("hidden");
+        document.querySelector(".example").classList.remove("hidden");
+        amount.textContent = data.totalNotValidRecords;
+
+        example.textContent = buildString(data.aspspValidationErrorReports);
+    } else {
+        verdict.classList.add("valid");
+        verdict.parentElement.classList.remove("hidden");
+        amount.parentElement.classList.add("hidden");
+        example.classList.add("hidden");
+        document.querySelector(".example").classList.add("hidden");
+    }
+
+}
+
+const buildString = (input) => {
+    let result = "";
+
+    for (let i = 0, limit = Math.min(3, input.length); i < limit; i++) {
+        result += "line number: " + input[i].lineNumberInCsv + "\n"
+            + "validation errors: \n";
+
+        input[i].validationErrors.forEach(element => {
+            result += "\t" + element + "\n";
+        });
+
+        result += "\n";
+    }
+
+    if (input[3]) {
+        result += "\n... ";
+    }
+
+    return result;
+}
