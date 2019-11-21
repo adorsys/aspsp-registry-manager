90d3144bc667383cb6415302bb84251aba0baba0
berezkin88 <berezkin05@gmail.com>
2019-11-21 15:25:37 +0200

    adjust report() function

--- registry-manager-rest-server/src/main/resources/static/main.js
+++ registry-manager-rest-server/src/main/resources/static/main.js
@@ -22,7 +22,7 @@ function initGlobals() {
 
 function validateBankName(element) {
     let target = element.textContent;
-    let regex = /^[\w\-\s\WäöüÄÖÜß]+$/;
+    let regex = /^[\w\s\WäöüÄÖÜß]+$/;
 
     if (!regex.test(target)) {
         element.classList.add("invalid");
@@ -679,8 +679,7 @@ function showButton() {
 const proceedButton = () => {
     if (HIT_BUTTON === "UPLOAD") {
         upload();
-    } 
-    if (HIT_BUTTON === "MERGE") {
+    } else if (HIT_BUTTON === "MERGE") {
         merge();
     }
 
